b92e2731f66e8a3d5850f99c7760b33c351ffee5
Sarthak <sarthakbansalog@gmail.com>
2025-05-22 19:56:29 +0530

    Initialised files for landing page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,7 +3,10 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import About from "./pages/about/about";
+import Header from "./components/Header";
 
 const queryClient = new QueryClient();
 
@@ -16,6 +19,8 @@ const App = () => (
             <Sonner />
             <Routes>
               <Route path="/" element={<Index />} />
+              <Route path="/about" element={<About />} />
+              <Route path="*" element={<NotFound />} />
             </Routes>
           </div>
         </div>
