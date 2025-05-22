9c38da2cb51a33a43d3ec5f20fa8056bad2162f8
Sarthak <sarthakbansalog@gmail.com>
2025-05-22 22:07:46 +0530

    Added new routes and css

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -5,6 +5,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import Marketplace from "./pages/marketplace/marketplace";
+import Admin from "./pages/admin/admin";
+import Issuer from "./pages/Issuer/issuer";
 import About from "./pages/about/about";
 import Header from "./components/Header";
 
@@ -19,6 +22,9 @@ const App = () => (
             <Sonner />
             <Routes>
               <Route path="/" element={<Index />} />
+              <Route path="/marketplace" element={<Marketplace />} />
+              <Route path="/admin" element={<Admin />} />
+              <Route path="/issuer" element={<Issuer />} />
               <Route path="/about" element={<About />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
